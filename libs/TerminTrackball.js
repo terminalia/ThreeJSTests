a356abea16ddcef190cef2c7d7c0ecefa5123e75
terminalia <l.zanconi@todo.to.it>
2017-06-01 17:04:20 +0200

    Add text to texture

--- libs/TerminTrackball.js
+++ libs/TerminTrackball.js
@@ -4,36 +4,55 @@
  */
 window.TERMINALIA = window.TERMINALIA || {};
 
-TERMINALIA.Trackball = function Trackball() {
+TERMINALIA.Trackball = function Trackball(rot_offset) {
 
     var self = this;
     self.last_x = 0;
     self.last_y = 0;
     self.rot_x = 0;
     self.rot_y = 0;
-    self.leftPressed = false;
     self.object = null;
+    self.leftPressed = false;
+    self.offset = 20;
+    self.enabled = true;
 
-    function rotateObject(enabled, x, y) {
-        console.log(x,y)
-        var diff_x = x - self.last_x;
-        var diff_y = y - self.last_y;
+    function onMouseDown(x, y) {
+        self.leftPressed = true;
         self.last_x = x;
         self.last_y = y;
+        console.log("Mouse Down", self.leftPressed);
+    }
 
-        if (enabled) {
-            self.rot_x += 0.005 * diff_x;
-            self.rot_y += 0.005 * diff_y;
+    function onMouseUp() {
+        self.leftPressed = false;
+    }
 
-            console.log(self.last_x, self.last_y);
-            self.object.rotation.set(self.rot_y, self.rot_x, 0);
+    function onMouseMove(x, y) {
+        var diff_x = x - last_x;
+        var diff_y = y - last_y;
+        last_x = x;
+        last_y = y;
+
+        if (self.leftPressed && self.enabled) {
+            console.log("Mouse Move");
+            rot_x += diff_x;
+            rot_y += diff_y;
+
+            self.object.rotation.set(radians(self.offset), radians(rot_x), 0);
         }
     }
 
     function addRotationToObject(object) {
         self.object = object;
+        console.log(self.object);
     }
 
-    self.rotateObject = rotateObject;
+    function radians(degrees) {
+        return degrees * Math.PI / 180;
+    }
+
+    self.onMouseDown = onMouseDown;
+    self.onMouseUp = onMouseUp;
+    self.onMouseMove = onMouseMove;
     self.addRotationToObject = addRotationToObject;
 }
